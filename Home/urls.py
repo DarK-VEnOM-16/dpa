165a9cd1351e156b31c1bcff827537db4f05052a
DarK-VEnOM-16 <75152702+DarK-VEnOM-16@users.noreply.github.com>
2021-07-31 17:58:02 +0530

    Second Commit
    Signed-off-by: DarK-VEnOM-16 <75152702+DarK-VEnOM-16@users.noreply.github.com>

--- Home/urls.py
+++ Home/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     path(r"",views.index,name="Home"),
-    path(r"about",views.about,name="about"),
-    path(r"service",views.services,name="services"),
+    path(r"europe/",views.europe,name="about"),
+    path(r"asia/",views.asia,name="about"),
+    path(r"australia/",views.australia,name="about"),
+    path(r"southamerica/",views.southamerica,name="about"),
+    path(r"northamerica/",views.northamerica,name="services"),
     path(r"contact",views.contact,name="contact"),
 
     path(r'^(?P<album_id>[0-9]+$)/',views.details,name="details")
--- Home/views.py
+++ Home/views.py
@@ -2,15 +2,46 @@ from typing import ContextManager
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import data
+
 def index(request):
     content={
-        'length':len(data.objects.all()),
+        'cont'  : 'all',
         'data'  : data.objects.all()
     }
     return render(request,'index1.html',content)
 
-def about(request):
-    return HttpResponse("<h1>Hello~~~World</h1><h1>How are you</h1><h1>This is about page</h1>")
+def europe(request):
+    content={
+        'cont'  : "eu",
+        'data'  : data.objects.filter(continent="United Kingdom")
+    }
+    return render(request,'index1.html',content)    
+def australia(request):
+    content={
+        'cont'  : "aus",
+        'data'  : data.objects.filter(continent="Australia")
+    }
+    return render(request,'index1.html',content)    
+def asia(request):
+    content={
+        'cont'  : "as",
+        'data'  : data.objects.filter(continent="Asia")
+    }
+    return render(request,'index1.html',content)    
+def northamerica(request):
+    content={
+        'cont'  : "na",
+        'data'  : data.objects.filter(continent="northamerica")
+    }
+    return render(request,'index1.html',content)    
+def southamerica(request):
+    content={
+        'cont'  : "sa",
+        'data'  : data.objects.filter(continent="southamerica")
+    }
+    return render(request,'index1.html',content)    
+
+
 
 def services(request):
     return HttpResponse("<h1>Hello~~~World</h1><h1>How are you</h1><h1>This is Service page</h1>")
